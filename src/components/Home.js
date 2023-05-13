f45c6fb66903fd735e8a1d1006f4f6dcdfface05
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-05-13 12:00:01 +0530

    add input and button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,20 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return <div>
+    <h1 className=" text-4xl font-extrabold py-5">Task Master</h1>
+    <p className=" text-sm font-thin px-16">
+    The task master is organize my task it helap you to to do your work perfectlykjchdiu uif ffuif fhiufhihf u fuhruf fuqfihq uirf fuifrui fuiuru
+    </p>
+
+{/* add task  */}
+
+<section>
+    <input type="text" placeholder="Add new task" className="border-2 border-purple-300 p-2 m-2 w-60 rounded-md"/>
+    <button className=" bg-yellow-400 p-2 "> Add task</button>
+</section>
+
+  </div>;
 }
 
 export default Home;
